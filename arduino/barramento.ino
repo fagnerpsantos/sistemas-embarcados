9350094862c18ead897dc19a3ef98a1a40b17e11
anapandrade <anaandrade.456@gmail.com>
2015-11-08 20:39:20 -0300

    Adicionando arquivos do arduino

--- arduino/barramento.ino
+++ arduino/barramento.ino
@@ -1,34 +1,50 @@
+/********************************************************************************
+* ADXL345 Library Examples- pitch_roll.ino                                      *
+*                                                                               *
+* Copyright (C) 2012 Anil Motilal Mahtani Mirchandani(anil.mmm@gmail.com)       *
+*                                                                               *
+* License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html> *
+* This is free software: you are free to change and redistribute it.            *
+* There is NO WARRANTY, to the extent permitted by law.                         *
+*                                                                               *
+*********************************************************************************/
+
 #include <Wire.h>
 #include "ADXL345.h"
 
-ADXL345 acel =  ADXL345();
+const float alpha = 0.5;
 
-struct Eixos {
-  int acelX, acelY, acelZ;
-  
-};
+double fXg = 0;
+double fYg = 0;
+double fZg = 0;
 
-Eixos eixos;
+ADXL345 acc;
 
-void setup() {
-  Serial.begin(9600);
-  acel.powerOn(); 
-};
+void setup()
+{
+	acc.begin();
+	Serial.begin(9600);
+	delay(100);
+}
 
-void enviarEixos() {
-  int tam = sizeof(eixos);
-  char buff(tam);
-  
-  memcpy(&buff, &eixos, tam);
-  
-  Serial.write('I');
-  Serial.write((uint8_t*)&buff, tam);
-  Serial.write('F'); 
-};
 
-void loop() {
-  acel.readAccel(&eixos.acelX, &eixos.acelY, &eixos.acelZ);
-  enviarEixos();
-  
-  delay(50);
-};
+void loop()
+{
+	double pitch, roll, Xg, Yg, Zg;
+	acc.read(&Xg, &Yg, &Zg);
+
+	//Low Pass Filter
+	fXg = Xg * alpha + (fXg * (1.0 - alpha));
+	fYg = Yg * alpha + (fYg * (1.0 - alpha));
+	fZg = Zg * alpha + (fZg * (1.0 - alpha));
+
+	//Roll & Pitch Equations
+	roll  = (atan2(-fYg, fZg)*180.0)/M_PI;
+	pitch = (atan2(fXg, sqrt(fYg*fYg + fZg*fZg))*180.0)/M_PI;
+
+	Serial.print(pitch);
+	Serial.print(":");
+	Serial.println(roll);
+
+	delay(500);
+}
