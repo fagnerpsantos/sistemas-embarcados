744f87618a08ccd81766a3d4c03f9692d3a5c864
anapandrade <anaandrade.456@gmail.com>
2015-11-08 20:19:42 -0300

    Adicionando codigo c++

---
+++ cpp/comunicacaoSensores/Release/src/Comunicacao.d
@@ -0,0 +1,4 @@
+src/Comunicacao.d: ../src/Comunicacao.cpp \
+ /home/fagnerpsantos/Desenvolvimento/workspace\ arduino/comunicacaoSensores/include/Comunicacao.h
+
+/home/fagnerpsantos/Desenvolvimento/workspace\ arduino/comunicacaoSensores/include/Comunicacao.h:
---
+++ cpp/comunicacaoSensores/include/Extern.h
@@ -0,0 +1,27 @@
+/*
+ * External.h
+ *
+ *  Created on: 15/10/2015
+ *      Author: fagnerpsantos
+ */
+
+#ifndef INCLUDE_EXTERN_H_
+#define INCLUDE_EXTERN_H_
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+int iniciar(char* porta);
+int ler();
+int getAcelX();
+int getAcelY();
+int getAcelZ();
+int finalizar();
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /* INCLUDE_EXTERN_H_ */
